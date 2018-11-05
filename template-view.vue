035ca714271fd200dff3ce72c877d52113445f59
不见长安 <31404297+seechangan@users.noreply.github.com>
2018-11-05 18:05:15 +0800

    Add files via upload

---
+++ template-view.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+	         <views ref="scroll"  :scrollX="ThisX" :scrollY="ThisY" :data="ThisData">
+		<div  class="scroll-content"></div>
+	          </views>
+	</div>
+</template>
+
+export default {
+  components: {
+   
+    views
+  },
+  data() {
+    return {
+       ThisX:false,//控制横向是否可滚动 
+       ThisY:true,//控制竖向是否可滚动 
+       ThisData:[],//根据solt中接收时所规定的类型来传递，默认是数组
+    };
+  }
+
+
+
+};
+
